2b3d40634e59673e832c3c4a6cccf4a65f876b75
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-09-27 13:35:47 -0400

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const newArray = [];
+    for(let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    };
+};
+
+countDown();
